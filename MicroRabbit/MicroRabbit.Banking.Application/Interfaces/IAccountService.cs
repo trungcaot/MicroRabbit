78cddbd545119535a12393ef2f1d86cc408b4e62
trungct <trungcaot@hotmail.com>
2019-10-09 23:07:34 +0700

    Paritally completed Banking Microservice.

---
+++ MicroRabbit/MicroRabbit.Banking.Application/Interfaces/IAccountService.cs
@@ -0,0 +1,10 @@
+﻿using MicroRabbit.Banking.Domain.Models;
+using System.Collections.Generic;
+
+namespace MicroRabbit.Banking.Application.Interfaces
+{
+    public interface IAccountService
+    {
+        IEnumerable<Account> GetAccounts();
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -0,0 +1,22 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using System.Collections.Generic;
+
+namespace MicroRabbit.Banking.Application.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountService(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _accountRepository.GetAccounts();
+        }
+    }
+}
